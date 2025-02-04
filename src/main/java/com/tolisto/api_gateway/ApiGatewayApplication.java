3e49e1d009a65b4434ec86c8b318281610ecbd34
MrForero <foreroyatesantiago@outlook.com>
2025-02-03 21:11:14 -0500

    pending revisions

--- src/main/java/com/tolisto/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/tolisto/api_gateway/ApiGatewayApplication.java
@@ -2,10 +2,11 @@ package com.tolisto.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "org.springframework.security.oauth2.jwt")
 //@SpringBootApplication
-public class ApiGatewayApplication {
+public class ApiGatewayApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
